1bf7b12b96c2dd58d69e68709f0852f212a05570
Aimarall <aimaral21072004@gmail.com>
2021-04-20 15:41:47 +0600

    added functions to this file

---
+++ src/redux/counterReducer.js
@@ -0,0 +1,15 @@
+const intialState = {
+    number: 0,
+}
+const counterReduce = (state = intialState, action) => {
+    switch (action.type) {
+        case "increment_by_1":
+
+            break;
+
+        default:
+            break;
+    }
+    return state;
+}
+export default counterReduce;
