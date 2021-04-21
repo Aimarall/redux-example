6f8c20041136735bc72e1ea01d401aca24b27c9e
Aimarall <aimaral21072004@gmail.com>
2021-04-21 13:02:21 +0600

    added nuttons

--- src/components/CounterControls.js
+++ src/components/CounterControls.js
@@ -1,10 +1,36 @@
-const CounterDisplay = () => {
+import { useDispatch } from "react-redux";
 
-    
-    return (
+
+const CounterControls = () =>{
+    const dispatch = useDispatch();
+    function incrementBy1Callback(){
+        dispatch({type: "increment_by_1"});
+    }
+    function incrementBy10Callback() {
+        dispatch({type: "increment_by_10"});
+    }
+    function incrementBy10Callback() {
+        dispatch({type: "increment_by_10"});
+    }
+    function incrementBy10Callback() {
+        dispatch({type: "increment_by_10"});
+    }
+    function incrementBy10Callback() {
+        dispatch({type: "increment_by_10"});
+    }
+
+    return(
         <div>
-            <button>+1</button>
+            <button onClick={incrementBy1Callback}>+1</button>
+            <button  onClick={incrementBy10Callback}>+10</button>
+            <button  onClick={incrementBy10Callback}>-10</button>
+
+            <button  onClick={incrementBy10Callback}>*10</button>
+            <button  onClick={incrementBy10Callback}>/10</button>
+
+            <button  onClick={incrementBy10Callback}>-</button>
+            <button  onClick={incrementBy10Callback}>+</button>
         </div>
     );
 }
-export default CounterDisplay;
+export default CounterControls;
--- src/redux/counterReducer.js
+++ src/redux/counterReducer.js
@@ -2,14 +2,34 @@ const intialState = {
     number: 0,
 }
 const counterReduce = (state = intialState, action) => {
+    const newState = { ...state };
+
     switch (action.type) {
         case "increment_by_1":
+            newState.number++;
+            return newState;
 
-            break;
+        case "increment_by_10":
+            newState.number += 10;
+            return newState;
+
+        case "increment_by_10":
+            newState.number -= 10;
+            return newState;
+
+        case "increment_by_2":
+            newState.number *= 10;
+            return newState;
+
+        case "increment_by_2":
+            newState.number /= 10;
+            return newState;
+
+      
 
         default:
             break;
     }
     return state;
 }
-export default counterReduce;
+export default counterReduce
