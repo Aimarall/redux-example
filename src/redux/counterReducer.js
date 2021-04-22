6f1e48c770593da45d4e54846c3b43928a99837e
Aimarall <aimaral21072004@gmail.com>
2021-04-22 14:06:25 +0600

    added functions and color

--- src/redux/counterReducer.js
+++ src/redux/counterReducer.js
@@ -13,37 +13,52 @@
 //             newState.number += 10;
 //             return newState;
 
-//         case "increment_by_10":
-//             newState.number -= 10;
+//         case "decrease_by_1":
+//             newState.number--;
 //             return newState;
 
-//         case "increment_by_2":
-//             newState.number *= 10;
+//         case "multiplication_by_2":
+//             newState.number *= 2;
 //             return newState;
 
-//         case "increment_by_2":
-//             newState.number /= 10;
+//         case "division_by_2":
+//             newState.number /= 2;
 //             return newState;
 
+//         case "reset_by_2":
+//             newState.number = 0;
+//             return newState;
 
+//         case "random_by_2":
+//             newState.number = Math.floor(Math.random() * 100) + 1;
+//             return newState;
 
 //         default:
 //             break;
 //     }
 //     return state;
 // }
-// export default counterReduce
+// export default counterReduce;
+
+
+
+
 
 
 
 
 const intialState = {
     number: 0,
+    color: "#000000"
 }
-const counterReduce = (state = intialState, action) => {
+const counterReducer = (state = intialState, action) => {
     const newState = { ...state };
 
     switch (action.type) {
+        case "change_color":
+            newState.color = action.color;
+            return newState;
+
         case "increment_by_1":
             newState.number++;
             return newState;
@@ -77,4 +92,4 @@ const counterReduce = (state = intialState, action) => {
     }
     return state;
 }
-export default counterReduce;
+export default counterReducer;
