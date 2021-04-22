717e614b4e5ca3280cd687721721bfb6a0986637
Aimarall <aimaral21072004@gmail.com>
2021-04-22 14:57:33 +0600

    added components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import ColorDisplay from "./components/ColorDisplay";
 import ColorSimpleControls from "./components/ColorSimpleControls";
+import ColorSliderControls from "./components/ColorSliderControls";
+
 
 function App() {
   return (
     <div className="App">
       <ColorDisplay />
       <ColorSimpleControls />
-
-
+      <ColorSliderControls />
     </div>
   );
 }
 
 export default App;
+
+
