0c7592e02215ec62006e83d6f525bf9230787f0c
Aimarall <aimaral21072004@gmail.com>
2021-04-22 14:06:16 +0600

    added functions and color

--- src/components/CounterControls.js
+++ src/components/CounterControls.js
@@ -1,35 +1,82 @@
+// // import { useDispatch } from "react-redux";
+
+
+// // const CounterControls = () =>{
+// //     const dispatch = useDispatch();
+// //     function incrementBy1Callback(){
+// //         dispatch({type: "increment_by_1"});
+// //     }
+// //     function incrementBy10Callback() {
+// //         dispatch({type: "increment_by_10"});
+// //     }
+// //     function incrementBy10Callback() {
+// //         dispatch({type: "increment_by_10"});
+// //     }
+// //     function incrementBy10Callback() {
+// //         dispatch({type: "increment_by_10"});
+// //     }
+// //     function incrementBy10Callback() {
+// //         dispatch({type: "increment_by_10"});
+// //     }
+
+// //     return(
+// //         <div>
+// //             <button onClick={incrementBy1Callback}>+1</button>
+// //             <button  onClick={incrementBy10Callback}>+10</button>
+// //             <button  onClick={incrementBy10Callback}>-10</button>
+
+// //             <button  onClick={incrementBy10Callback}>*10</button>
+// //             <button  onClick={incrementBy10Callback}>/10</button>
+
+// //             <button  onClick={incrementBy10Callback}>-</button>
+// //             <button  onClick={incrementBy10Callback}>+</button>
+// //         </div>
+// //     );
+// // }
+// // export default CounterControls;
+
+
+
 // import { useDispatch } from "react-redux";
 
 
 // const CounterControls = () =>{
 //     const dispatch = useDispatch();
+
 //     function incrementBy1Callback(){
 //         dispatch({type: "increment_by_1"});
 //     }
 //     function incrementBy10Callback() {
 //         dispatch({type: "increment_by_10"});
 //     }
-//     function incrementBy10Callback() {
-//         dispatch({type: "increment_by_10"});
+
+//     function decreaseBy1Callback() {
+//         dispatch({type: "decrease_by_1"});
 //     }
-//     function incrementBy10Callback() {
-//         dispatch({type: "increment_by_10"});
+//     function multiplicationBy2Callback() {
+//         dispatch({type: "multiplication_by_2"});
 //     }
-//     function incrementBy10Callback() {
-//         dispatch({type: "increment_by_10"});
+//     function divisionBy2Callback() {
+//         dispatch({type: "division_by_2"});
 //     }
 
+//     function resetBy2Callback() {
+//         dispatch({type: "reset_by_2"});
+//     }
+//     function randomBy2Callback() {
+//         dispatch({type: "random_by_2"});
+//     }
 //     return(
 //         <div>
 //             <button onClick={incrementBy1Callback}>+1</button>
 //             <button  onClick={incrementBy10Callback}>+10</button>
-//             <button  onClick={incrementBy10Callback}>-10</button>
+//             <button  onClick={decreaseBy1Callback}>-1</button>
+//             <button  onClick={multiplicationBy2Callback}>*2</button>
+//             <button  onClick={divisionBy2Callback}>/2</button>
 
-//             <button  onClick={incrementBy10Callback}>*10</button>
-//             <button  onClick={incrementBy10Callback}>/10</button>
+//             <button  onClick={resetBy2Callback}>Reset</button>
+//             <button  onClick={randomBy2Callback}>Random</button>
 
-//             <button  onClick={incrementBy10Callback}>-</button>
-//             <button  onClick={incrementBy10Callback}>+</button>
 //         </div>
 //     );
 // }
@@ -37,45 +84,48 @@
 
 
 
+
+
+
 import { useDispatch } from "react-redux";
 
 
-const CounterControls = () =>{
+const CounterControls = () => {
     const dispatch = useDispatch();
-    
-    function incrementBy1Callback(){
-        dispatch({type: "increment_by_1"});
+
+    function incrementBy1Callback() {
+        dispatch({ type: "increment_by_1" });
     }
     function incrementBy10Callback() {
-        dispatch({type: "increment_by_10"});
+        dispatch({ type: "increment_by_10" });
     }
-    
+
     function decreaseBy1Callback() {
-        dispatch({type: "decrease_by_1"});
+        dispatch({ type: "decrease_by_1" });
     }
     function multiplicationBy2Callback() {
-        dispatch({type: "multiplication_by_2"});
+        dispatch({ type: "multiplication_by_2" });
     }
     function divisionBy2Callback() {
-        dispatch({type: "division_by_2"});
+        dispatch({ type: "division_by_2" });
     }
 
     function resetBy2Callback() {
-        dispatch({type: "reset_by_2"});
+        dispatch({ type: "reset_by_2" });
     }
     function randomBy2Callback() {
-        dispatch({type: "random_by_2"});
+        dispatch({ type: "random_by_2" });
     }
-    return(
+    return (
         <div>
             <button onClick={incrementBy1Callback}>+1</button>
-            <button  onClick={incrementBy10Callback}>+10</button>
-            <button  onClick={decreaseBy1Callback}>-1</button>
-            <button  onClick={multiplicationBy2Callback}>*2</button>
-            <button  onClick={divisionBy2Callback}>/2</button>
+            <button onClick={incrementBy10Callback}>+10</button>
+            <button onClick={decreaseBy1Callback}>-1</button>
+            <button onClick={multiplicationBy2Callback}>*2</button>
+            <button onClick={divisionBy2Callback}>/2</button>
 
-            <button  onClick={resetBy2Callback}>Reset</button>
-            <button  onClick={randomBy2Callback}>Random</button>
+            <button onClick={resetBy2Callback}>Reset</button>
+            <button onClick={randomBy2Callback}>Random</button>
 
         </div>
     );
